653b13028e4518f4ffc5812af1fda43f6ca03e20
Vitalij Saiganov <vitalij.saiganov@1pt.com>
2022-03-09 18:14:56 +0300

    Add weight creation

--- Reel.py
+++ Reel.py
@@ -1,12 +1,16 @@
 import numpy as np
+from collections import defaultdict
 
 class Reel():
     def __init__(self, ind):
         self.symbols = []
+        self.symbol_in_stacks = []
         self.weights = []
         self.ln = 0
         self.number_of_stacks = 0
         self.index = ind
+        self.pattern_counter = defaultdict(int)
+        self.pattern_indexes = defaultdict(list)
 
 
     def SetSymbols(self, symbols):
@@ -15,7 +19,140 @@ class Reel():
         self.weights = [1 for _ in range(self.ln)]
 
 
-    def MakeReelofCommonSymbols(self, common_symbols, window_height):
+    def MakeReel(self, common_symbols, sp_symbols, dist_between_sp_symbols, window_height):
+        ready_reel_without_sp_symbols = self._MakeReelofCommonSymbols(common_symbols, window_height)
+        ready_reel_with_sp_symbols = self._InsertSpecialSymbols(ready_reel_without_sp_symbols,
+                                                                sp_symbols,
+                                                                dist_between_sp_symbols)
+        for stack in ready_reel_with_sp_symbols:
+            self.symbol_in_stacks.append(stack)
+        self._UnpackSymbolStacks()
+
+
+
+    def MakeWeights(self, weight_patterns, weight_percentage, window_height):
+        self._FindPatternsInReels(weight_patterns, window_height)
+        self._AddWeightForPatterns(weight_patterns, weight_percentage)
+
+
+    def _PrintPattern(self, pattern):
+        for i, row_pat in enumerate(pattern):
+            print("Row ", i, ": ",row_pat , sep='')
+
+    def _CheckWeightPatternsCounter(self,):  # True - все ок, False - каие-то паттерны отсутствуют
+        no_pattern_indexes = []
+        for pattern_index, pattern_counter in self.pattern_counter:
+            if pattern_counter == 0:
+                no_pattern_indexes.append(pattern_index)
+        print("In reel no such patterns:")
+
+
+    def _FindPattern(self, window, patterns):
+        pattern_bool_vector = [False for _ in range(len(patterns))]
+        for i, pattern in enumerate(patterns):
+            window_same_to_pattern = True
+            for j, row_pattern in enumerate(pattern):
+                if window[j] in row_pattern:
+                    continue
+                else:
+                    window_same_to_pattern = False
+                    break
+            if window_same_to_pattern:
+                pattern_bool_vector[i] = True
+        if sum(pattern_bool_vector) > 1:
+            print("ERROR: Same weight patterns in reel", self.index)
+            return 0
+        return pattern_bool_vector
+
+
+    def _FindPatternsInReels(self, weight_patterns, window_height):
+        for i, pattern in weight_patterns:
+            self.pattern_counter[i] = 0
+        for i in range(len(self.symbols)):
+            window = self.TakeWindow(i, window_height)
+            weight_bool_pattern_vec = self._FindPattern(window, weight_patterns)
+            if sum(weight_bool_pattern_vec) == 0:
+                self.pattern_counter[-1] += 1  # Под ключем -1 находятся все окна не подходящие ни под один из паттернов
+                self.pattern_indexes[-1].append(i)
+            else:
+                true_index = weight_bool_pattern_vec.index(True)
+                self.pattern_counter[true_index] += 1
+                self.pattern_indexes[true_index].append(i)
+
+
+    def _CompareWeightsAndPatternCounter(self, weight_of_patterns):
+        for i, weight in enumerate(weight_of_patterns):
+            if weight < self.pattern_counter[i]:
+                return False
+        return True
+
+
+    def _AddWeightForPatterns(self, weight_patterns, weight_percentage, weight_to_input=1000):
+        if len(weight_percentage) != len(weight_patterns):
+            print("ERROR: Number of weight patterns not equal to number of weight percents in reel", self.index)
+            return 0
+        percents_sum = sum(weight_percentage)
+        if percents_sum > 100:
+            print("ERROR: Sum of percents in reel", self.index, "more than 100")
+            return 0
+
+        percents_of_not_patterns = 100 - percents_sum
+        self.weights = [0 for _ in range(self.ln)]
+
+        weight_of_not_patterns = int(weight_to_input * percents_of_not_patterns / 100)
+        weights_of_patterns = []
+        for percent in weight_percentage:
+            weights_of_patterns.append(percent * weight_to_input / 100)
+        while (weight_of_not_patterns < self.pattern_counter[-1] and
+               (not self._CompareWeightsAndPatternCounter(weights_of_patterns))):
+            weight_to_input *= 10
+            weight_of_not_patterns = int(weight_to_input * percents_of_not_patterns / 100)
+            weights_of_patterns = []
+            for percent in enumerate(weight_percentage):
+                weights_of_patterns.append(percent * weight_to_input / 100)
+        print(weight_to_input)
+
+        while(weight_of_not_patterns != 0):
+            for window_index in self.pattern_indexes[-1]:
+                self.weights[window_index] += 1
+                weight_of_not_patterns -= 1
+                if weight_of_not_patterns == 0:
+                    break
+
+        for pattern_index, pattern_index_in_reel in self.pattern_indexes.items():
+            weight = weights_of_patterns[pattern_index]
+            while(weight != 0):
+                for window_index in pattern_index_in_reel:
+                    self.weights[window_index] += 1
+                    weight -= 1
+                    if weight == 0:
+                        break
+
+
+    def TakeWindow(self,i, window_height):
+        start_index = i
+        end_index = i + window_height
+        if i > self.ln:
+            i %= self.ln
+            end_index = i + window_height
+        if end_index > self.ln:
+            first_part = self.symbols[i:]
+            second_part = self.symbols[:window_height - len(first_part)]
+            return first_part + second_part
+        else:
+            return self.symbols[i:i + window_height]
+
+
+    def _UnpackSymbolStacks(self):
+        for stack in self.symbol_in_stacks:
+            self.symbols += [stack[1] for _ in range(stack[0])]
+
+
+    def PrintReel(self):
+        pass
+
+
+    def _MakeReelofCommonSymbols(self, common_symbols, window_height):
         not_shuffled_reel = self._CreateNotShuffledReelWithoutSpSymbols(common_symbols)
         shuffled_reel = self._RandomShuffle(not_shuffled_reel)
         need_change_reel = False
@@ -42,23 +179,17 @@ class Reel():
                     need_change_reel = True
                     break
         if need_change_reel:
-            shuffled_reel = self.MakeReelofCommonSymbols(common_symbols, window_height)
+            shuffled_reel = self._MakeReelofCommonSymbols(common_symbols, window_height)
         return shuffled_reel
 
 
-    def InsertSpecialSymbols(self, ready_reel, special_symbols, dist_bet_sp_symbols):
+    def _InsertSpecialSymbols(self, ready_reel, special_symbols, dist_bet_sp_symbols):
         sp_symbols = []
         indexes_between_huge_stacks = []
-        for i, stack in ready_reel[:-dist_bet_sp_symbols]:
+
+        for i, stack in enumerate(ready_reel):
             if (stack[0] > 1) and (ready_reel[(i+1) % self.number_of_stacks][0] > 1):
-                if not indexes_between_huge_stacks:
-                    indexes_between_huge_stacks.append(i + 1)
-                    continue
-                dist_bt_indexes = 0
-                for st in ready_reel(indexes_between_huge_stacks[-1], i+1):
-                    dist_bt_indexes += st[0]
-                if dist_bt_indexes >= dist_bet_sp_symbols:
-                    indexes_between_huge_stacks.append(i+1)
+                indexes_between_huge_stacks.append(i+1)
 
         sp_symbols_list = []
         for symbol, stacks in special_symbols.items():
@@ -67,36 +198,43 @@ class Reel():
                 for i in range(stack[0]):
                     sp_symbols_list.append([stack[1], symbol])
 
-
-        ## Работает ли эта тема правильно большой вопрос
-        while len(sp_symbols_list) != 0 and len(indexes_between_huge_stacks) != 0:
-            inserted_indexes = []
-            rand_sp_stack_index = np.random.choice(*range(sp_symbols_list))
-            del sp_symbols_list[rand_sp_stack_index]
-            rand_place_index = np.random.choice(*range(indexes_between_huge_stacks))
-            inserted_indexes.append(indexes_between_huge_stacks[rand_place_index])
-            ready_reel.insert(indexes_between_huge_stacks[rand_place_index]
-                              + sum([ind for ind in inserted_indexes if ind < indexes_between_huge_stacks[rand_place_index]]),
-                              sp_symbols_list[rand_sp_stack_index])
-            del indexes_between_huge_stacks[rand_place_index]
-
+        ready_reel, sp_symbols_list = self._InsertSpSymbols(ready_reel,
+                                                            indexes_between_huge_stacks,
+                                                            sp_symbols_list,
+                                                            sp_symbols,
+                                                            dist_bet_sp_symbols)
 
         if len(sp_symbols_list) == 0:
             return ready_reel
 
-        for i, stack in ready_reel:
+        indexes_to_input_sp_symbols = []
+        for i, stack in enumerate(ready_reel):
+            if self._CanIInsertSpSymbolInthisIndex(ready_reel, i, sp_symbols, dist_bet_sp_symbols + 1):
+                indexes_to_input_sp_symbols.append(i)
+
+        ready_reel, sp_symbols_list = self._InsertSpSymbols(ready_reel,
+                                                            indexes_to_input_sp_symbols,
+                                                            sp_symbols_list,
+                                                            sp_symbols,
+                                                            dist_bet_sp_symbols)
+
+        if len(sp_symbols_list) != 0:
+            print("\nWARNING !!! In reel not enough place to input special symbols,", len(sp_symbols_list), "special symbols not inserted.")
+        return ready_reel
 
 
+    def _CanIInsertSpSymbolInthisIndex(self, reel, index, sp_symbols, dist_bet_sp_symbols):
+        left_neigh = self._LeftNeighbors(reel, index, dist_bet_sp_symbols+1)
+        right_neigh = self._RightNeighbors(reel, index, dist_bet_sp_symbols+1, True)
 
-
-
-
-    def MakeWeights(self, weight_patterns, weight_percentage):
-        pass
-
-
-    def PrintReel(self):
-        pass
+        good_place = True
+        for sp_symbol in sp_symbols:
+            if (sp_symbol not in left_neigh) and (sp_symbol not in right_neigh):
+                continue
+            else:
+                good_place = False
+                break
+        return good_place
 
 
     def _CreateNotShuffledReelWithoutSpSymbols(self, common_symbols):
@@ -134,10 +272,11 @@ class Reel():
         return left_neighbors
 
 
-    def _RightNeighbors(self, reel, stack_index, window_height):
+    def _RightNeighbors(self, reel, stack_index, window_height, change_or_insert = False): #False - change True - insert
         right_neighbors = []
+        step = (0 if change_or_insert else 1)
         while len(right_neighbors) != (window_height - 1):
-            right_stack = reel[(stack_index + 1) % self.number_of_stacks]
+            right_stack = reel[(stack_index + step) % self.number_of_stacks]
             for i in range(right_stack[0]):
                 right_neighbors.append(right_stack[1])
                 if len(right_neighbors) == (window_height - 1):
@@ -146,17 +285,43 @@ class Reel():
         return right_neighbors
 
 
+    def _InsertSpSymbols(self, reel, indexes_to_input, sp_symbols_list, sp_symbols, dist_bet_sp_symbols):
+        inserted_indexes = []
+        while len(sp_symbols_list) != 0 and len(indexes_to_input) != 0:
+
+            rand_sp_stack_index = np.random.choice([*range(len(sp_symbols_list))])
+            rand_place_index = np.random.choice([*range(len(indexes_to_input))])
+
+            if self._CanIInsertSpSymbolInthisIndex(reel, indexes_to_input[rand_place_index] +
+                                                               sum([1 for ind in inserted_indexes if
+                                                                    ind < indexes_to_input[
+                                                                        rand_place_index]]),
+                                                   sp_symbols, dist_bet_sp_symbols):
+                inserted_indexes.append(indexes_to_input[rand_place_index])
+                reel.insert(indexes_to_input[rand_place_index]
+                                  + sum(
+                    [1 for ind in inserted_indexes if ind < indexes_to_input[rand_place_index]]),
+                                  sp_symbols_list[rand_sp_stack_index])
+                self.ln += sp_symbols_list[rand_sp_stack_index][0]
+                self.number_of_stacks += 1
+                del sp_symbols_list[rand_sp_stack_index]
+            del indexes_to_input[rand_place_index]
+
+        return reel, sp_symbols_list
+
+
 r = Reel(0)
 d = {}
-d[0] = [[5,1], [2,2], [1,3]]
-d[1] = [[5,1], [2,2], [1,3]]
-d[2] = [[5,1], [2,2], [1,3]]
-d[3] = [[5,1], [2,2], [1,3]]
-d[4] = [[5,1], [2,2], [1,3]]
-d[5] = [[5,1], [2,2], [1,3]]
-#d[6] = [[5,1], [2,2], [1,3]]
+d[0] = [[3, 1], [2, 2], [1, 3]]
+d[1] = [[3, 1], [2, 2], [1, 3]]
+d[2] = [[3, 1], [2, 2], [1, 3]]
+d[3] = [[3, 1], [2, 2], [1, 3]]
 
-print(r.MakeReelofCommonSymbols(d, 5))
-print(r.ln)
-print(r.number_of_stacks)
-
+s = {}
+s[10] = [[20, 1]]
+r.MakeReel(d, s, 4, 3)
+percent = [10, 50, 35]
+patterns = [[[10], [2,3], [2,3]], [[1,2,3], [2], [3]], [[2], [3], [1,2,3]]]
+r.MakeWeights(patterns, percent, 3)
+print(r.symbols)
+print(r.weights)
