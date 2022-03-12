687c9c3c4bc73358ffe944541bfd96fbff617bc1
Vitalij Saiganov <vitalij.saiganov@1pt.com>
2022-03-12 10:24:37 +0300

    Os path

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Source.Read_input import ReadSettings, ReadReel, ReadReelsetSettings, ReadR
 from Source.ReelData import ReelData
 from Source.Reelset import Reelset
 from Source.Reel import Reel
+import os
 
 
 ###################################### НАДО ВОТ ЭТО ЗАДАТЬ ######################################
@@ -9,6 +10,11 @@ GAME_NAME = "PoL"
 SETTING_FILE_NAME = "symbol_weight_settings"             # Только имя файла в папке Settings, путь и расширение не надо
 REELS_FILE_NAME = "Reelset"                       # Только имя, путь и расширение не надо
 #################################################################################################
+
+os.chdir("Reels")
+if not os.path.isdir(GAME_NAME):
+     os.mkdir(GAME_NAME)
+os.chdir("..")
 
 
 
@@ -18,3 +24,7 @@ reel_data = ReadSettings("Settings"+"/"+GAME_NAME+"/"+SETTING_FILE_NAME+".txt",
 reelset = Reelset(reel_data)
 reelset.MakeReelSet()
 reelset.PrintReelset()
+
+
+
+print("Текущая деректория:", os.getcwd())
