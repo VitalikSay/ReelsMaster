39cdb55c36b8f5149ceef8dcdcd6e44d6156ab71
Vitaliy Sayganov <vitalij.saiganov@1pt.com>
2022-05-05 18:15:09 +0300

    New special play PoL + CF trigger new

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from Source.Reelset import Reelset
 start_time = time.time()
 
 ###################################### НАДО ВОТ ЭТО ЗАДАТЬ ######################################
-GAME_NAME = "PoL"
-INNER_DIRECTORY = ""   # Имя папки внутри основной папки GAME_NAME, если таковой нет, то просто оставляй пустую строчку ""
-SETTING_FILE_NAME = "special_play"             # Только имя файла в папке Settings, путь и расширение не надо
+GAME_NAME = "CommonFeature"
+INNER_DIRECTORY = "NewNewCMReels"   # Имя папки внутри основной папки GAME_NAME, если таковой нет, то просто оставляй пустую строчку ""
+SETTING_FILE_NAME = "CF_trigger"             # Только имя файла в папке Settings, путь и расширение не надо
 REELS_FILE_NAME = "before_elimination"                       # Только имя файла в папке Reels, путь и расширение не надо
 #################################################################################################
 
