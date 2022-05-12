172ad5782b518f49edcbcbcb2f566e93815dcbd2
Vitaliy Sayganov <vitalij.saiganov@1pt.com>
2022-05-12 18:41:57 +0300

    Trigger reelsets for Slap Shot

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ start_time = time.time()
 ###################################### НАДО ВОТ ЭТО ЗАДАТЬ ######################################
 GAME_NAME = "CommonFeature"
 INNER_DIRECTORY = "NewNewCMReels"   # Имя папки внутри основной папки GAME_NAME, если таковой нет, то просто оставляй пустую строчку ""
-SETTING_FILE_NAME = "CF_trigger"             # Только имя файла в папке Settings, путь и расширение не надо
-REELS_FILE_NAME = "before_elimination"                       # Только имя файла в папке Reels, путь и расширение не надо
+SETTING_FILE_NAME = "after_elimination"             # Только имя файла в папке Settings, путь и расширение не надо
+REELS_FILE_NAME = "CF_trigger_reelset"                       # Только имя файла в папке Reels, путь и расширение не надо
 #################################################################################################
 
 
@@ -19,3 +19,7 @@ reelset.MakeReelSet()
 reelset.PrintReelset()
 
 print("\nTime:", '{:.3f}'.format(time.time() - start_time), "seconds")
+
+
+
+
